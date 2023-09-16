d90a051decf49209bcc0eb803f8fab6163e50606
TrauNguyen <nguyenvanngoctrau@gmail.com>
2023-09-16 16:56:33 -0500

    first commit

---
+++ Arrays/ContainsDuplicateII.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//219. Contains Duplicate II
+
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var map: [Int:Int] = [:]
+        for i in 0..<nums.count {
+            if let temp = map[nums[i]] {
+                if i - temp <= k {
+                    return true
+                } else {
+                    map[nums[i]] = i
+                }
+            } else {
+                map[nums[i]] = i
+            }
+        }
+        return false
+    }
+}
